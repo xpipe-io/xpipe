7484befd2ed76050085f13c5ff0907b6a5ec9efa
Christopher Schnick <st141923@stud.uni-stuttgart.de>
2021-12-02 14:49:03 +0100

    Fix modules

--- extension/src/main/java/module-info.java
+++ extension/src/main/java/module-info.java
@@ -1,6 +1,14 @@
+import io.xpipe.extension.DataSourceGuiProvider;
+import io.xpipe.extension.DataSourceProvider;
+import io.xpipe.extension.SupportedApplicationProvider;
+
 module io.xpipe.extension {
     requires io.xpipe.core;
     requires javafx.base;
 
     exports io.xpipe.extension;
+
+    uses DataSourceProvider;
+    uses DataSourceGuiProvider;
+    uses SupportedApplicationProvider;
 }
