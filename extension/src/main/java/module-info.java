568a6758a090a486dddc6b02fb3c8d4cd2ba6a5a
Christopher Schnick <st141923@stud.uni-stuttgart.de>
2021-12-02 14:50:06 +0100

    Add dependency

--- extension/src/main/java/module-info.java
+++ extension/src/main/java/module-info.java
@@ -5,6 +5,7 @@ import io.xpipe.extension.SupportedApplicationProvider;
 module io.xpipe.extension {
     requires io.xpipe.core;
     requires javafx.base;
+    requires javafx.graphics;
 
     exports io.xpipe.extension;
 
