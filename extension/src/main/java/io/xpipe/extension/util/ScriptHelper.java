1112803a781a8fe800c3dd5403b7d75757a77bf3
crschnick <crschnick@xpipe.io>
2023-02-04 08:40:15 +0000

    More fixes for mac

--- extension/src/main/java/io/xpipe/extension/util/ScriptHelper.java
+++ extension/src/main/java/io/xpipe/extension/util/ScriptHelper.java
@@ -1,7 +1,6 @@
 package io.xpipe.extension.util;
 
 import io.xpipe.core.impl.FileNames;
-import io.xpipe.core.process.OsType;
 import io.xpipe.core.process.ShellProcessControl;
 import io.xpipe.core.process.ShellType;
 import io.xpipe.core.process.ShellTypes;
@@ -16,7 +15,8 @@ import java.util.Random;
 public class ScriptHelper {
 
     public static int getScriptId() {
-        // A deterministic approach can cause permission problems when two different users execute the same command on a system
+        // A deterministic approach can cause permission problems when two different users execute the same command on a
+        // system
         // Therefore, use a random approach
         return new Random().nextInt(Integer.MAX_VALUE);
     }
@@ -30,64 +30,78 @@ public class ScriptHelper {
 
     private static final String ZSHI =
             """
-            #!/usr/bin/env zsh
+          #!/usr/bin/env zsh
 
-            emulate -L zsh -o no_unset
+          emulate -L zsh -o no_unset
 
-            if (( ARGC == 0 )); then
-              print -ru2 -- 'Usage: zshi <init-command> [zsh-flag]...
-            The same as plain `zsh [zsh-flag]...` except that an additional
-            <init-command> gets executed after all standard Zsh startup files
-            have been sourced.'
-              return 1
+          if (( ARGC == 0 )); then
+            print -ru2 -- 'Usage: zshi <init-command> [zsh-flag]...
+          The same as plain `zsh [zsh-flag]...` except that an additional
+          <init-command> gets executed after all standard Zsh startup files
+          have been sourced.'
+            return 1
+          fi
+
+          () {
+            local init=$1
+            shift
+            local tmp
+            {
+              tmp=$(mktemp -d ${TMPDIR:-/tmp}/zsh.XXXXXXXXXX) || return
+              local rc
+              for rc in .zshenv .zprofile .zshrc .zlogin; do
+                >$tmp/$rc <<<'{
+            if (( ${+_zshi_global_rcs} )); then
+              "builtin" "set" "-o" "global_rcs"
+              "builtin" "unset" "_zshi_global_rcs"
             fi
-
-            () {
-              local init=$1
-              shift
-              local tmp
-              {
-                tmp=$(mktemp -d ${TMPDIR:-/tmp}/zsh.XXXXXXXXXX) || return
-                local rc
-                for rc in .zshenv .zprofile .zshrc .zlogin; do
-                  >$tmp/$rc <<<'{
-              ZDOTDIR="$_zshi_zdotdir"
-              if [[ -f "$ZDOTDIR/'$rc'" && -r "$ZDOTDIR/'$rc'" ]]; then
-                "builtin" "source" "--" "$ZDOTDIR/'$rc'"
+            ZDOTDIR="$_zshi_zdotdir"
+            # Not .zshenv because /etc/zshenv has already been read
+            if [[ -o global_rcs && "'$rc'" != ".zshenv" && -f "/etc/'${rc:1}'" && -r "/etc/'${rc:1}'" ]]; then
+              "builtin" "source" "--" "/etc/'${rc:1}'"
+            fi
+            if [[ -f "$ZDOTDIR/'$rc'" && -r "$ZDOTDIR/'$rc'" ]]; then
+              "builtin" "source" "--" "$ZDOTDIR/'$rc'"
+            fi
+          } always {
+            if [[ -o "no_rcs" ||
+                  -o "login" && "'$rc'" == ".zlogin" ||
+                  -o "no_login" && "'$rc'" == ".zshrc" ||
+                  -o "no_login" && -o "no_interactive" && "'$rc'" == ".zshenv" ]]; then
+              if (( ${+_zshi_global_rcs} )); then
+                set -o global_rcs
               fi
+              "builtin" "unset" "_zshi_rcs" "_zshi_zdotdir"
+              "builtin" "command" "rm" "-rf" "--" '${(q)tmp}'
+              "builtin" "eval" '${(q)init}'
+            else
+              if [[ -o global_rcs ]]; then
+                _zshi_global_rcs=
+              fi
+              set -o no_global_rcs
+              _zshi_zdotdir=${ZDOTDIR:-~}
+              ZDOTDIR='${(q)tmp}'
+            fi
+          }' || return
+              done
+              _zshi_zdotdir=${ZDOTDIR:-~} ZDOTDIR=$tmp zsh "$@"
             } always {
-              if [[ -o "no_rcs" ||
-                    -o "login" && "'$rc'" == ".zlogin" ||
-                    -o "no_login" && "'$rc'" == ".zshrc" ||
-                    -o "no_login" && -o "no_interactive" && "'$rc'" == ".zshenv" ]]; then
-                "builtin" "unset" "_zshi_rcs" "_zshi_zdotdir"
-                "builtin" "command" "rm" "-rf" "--" '${(q)tmp}'
-                "builtin" "eval" '${(q)init}'
-              else
-                _zshi_zdotdir=${ZDOTDIR:-~}
-                ZDOTDIR='${(q)tmp}'
-              fi
-            }' || return
-                done
-                _zshi_zdotdir=${ZDOTDIR:-~} ZDOTDIR=$tmp zsh "$@"
-              } always {
-                [[ -e $tmp ]] && rm -rf -- $tmp
-              }
-            } "$@"
+              [[ -e $tmp ]] && rm -rf -- $tmp
+            }
+          } "$@"
             """;
 
-    public static String constructOpenWithInitScriptCommand(ShellProcessControl processControl, List<String> init, String toExecuteInShell) {
+    public static String constructOpenWithInitScriptCommand(
+            ShellProcessControl processControl, List<String> init, String toExecuteInShell) {
         ShellType t = processControl.getShellType();
         if (init.size() == 0 && toExecuteInShell == null) {
             return t.getNormalOpenCommand();
         }
 
         String nl = t.getNewLine().getNewLineString();
-        var content = String.join(nl, init)
-                + nl;
+        var content = String.join(nl, init) + nl;
 
-        if (processControl.getOsType().equals(OsType.LINUX)
-                || processControl.getOsType().equals(OsType.MAC)) {
+        if (t.equals(ShellTypes.BASH)) {
             content = "if [ -f ~/.bashrc ]; then . ~/.bashrc; fi\n" + content;
         }
 
@@ -101,7 +115,7 @@ public class ScriptHelper {
 
         if (t.equals(ShellTypes.ZSH)) {
             var zshiFile = createExecScript(processControl, ZSHI);
-            return t.getNormalOpenCommand() + " " + zshiFile + " " + initFile;
+            return t.getNormalOpenCommand() + " \"" + zshiFile + "\" \"" + initFile + "\"";
         }
 
         return t.getInitFileOpenCommand(initFile);
@@ -117,9 +131,7 @@ public class ScriptHelper {
     }
 
     @SneakyThrows
-    private static String createExecScript(
-            ShellProcessControl processControl, String file, String content
-    ) {
+    private static String createExecScript(ShellProcessControl processControl, String file, String content) {
         ShellType type = processControl.getShellType();
         content = type.prepareScriptContent(content);
 
@@ -136,9 +148,7 @@ public class ScriptHelper {
     }
 
     @SneakyThrows
-    public static String createAskPassScript(
-            SecretValue pass, ShellProcessControl parent, ShellType type
-    ) {
+    public static String createAskPassScript(SecretValue pass, ShellProcessControl parent, ShellType type) {
         var content = type.getScriptEchoCommand(pass.getSecretValue());
         var temp = parent.getTemporaryDirectory();
         var file = FileNames.join(temp, "askpass-" + getScriptId() + "." + type.getScriptFileEnding());
