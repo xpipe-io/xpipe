0f9cae06812765c0bc865142b4fbc69f31b0c91d
crschnick <crschnick@xpipe.io>
2024-01-06 16:54:57 +0000

    Rework comboboxes

--- app/src/main/java/io/xpipe/app/fxcomps/impl/ContextualFileReferenceChoiceComp.java
+++ app/src/main/java/io/xpipe/app/fxcomps/impl/ContextualFileReferenceChoiceComp.java
@@ -8,7 +8,6 @@ import io.xpipe.app.fxcomps.util.SimpleChangeListener;
 import io.xpipe.app.storage.DataStoreEntryRef;
 import io.xpipe.core.store.FileSystemStore;
 import javafx.beans.property.Property;
-import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.layout.HBox;
@@ -18,14 +17,12 @@ import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.util.List;
 
-public class FileReferenceChoiceComp extends SimpleComp {
+public class ContextualFileReferenceChoiceComp extends SimpleComp {
 
-    private final boolean hideFileSystem;
     private final Property<DataStoreEntryRef<? extends FileSystemStore>> fileSystem;
     private final Property<String> filePath;
 
-    public <T extends FileSystemStore> FileReferenceChoiceComp(ObservableValue<DataStoreEntryRef<T>> fileSystem, Property<String> filePath) {
-        this.hideFileSystem = true;
+    public <T extends FileSystemStore> ContextualFileReferenceChoiceComp(ObservableValue<DataStoreEntryRef<T>> fileSystem, Property<String> filePath) {
         this.fileSystem = new SimpleObjectProperty<>();
         SimpleChangeListener.apply(fileSystem, val -> {
             this.fileSystem.setValue(val);
@@ -33,27 +30,15 @@ public class FileReferenceChoiceComp extends SimpleComp {
         this.filePath = filePath;
     }
 
-    public FileReferenceChoiceComp(boolean hideFileSystem, Property<DataStoreEntryRef<? extends FileSystemStore>>  fileSystem, Property<String> filePath) {
-        this.hideFileSystem = hideFileSystem;
-        this.fileSystem = fileSystem != null ? fileSystem : new SimpleObjectProperty<>();
-        this.filePath = filePath;
-    }
-
     @Override
     protected Region createSimple() {
-        var fileSystemChoiceComp =
-                new FileSystemStoreChoiceComp(fileSystem).grow(false, true).styleClass(Styles.LEFT_PILL);
-        if (hideFileSystem) {
-            fileSystemChoiceComp.hide(new SimpleBooleanProperty(true));
-        }
-
         var fileNameComp = new TextFieldComp(filePath)
                 .apply(struc -> HBox.setHgrow(struc.get(), Priority.ALWAYS))
-                .styleClass(hideFileSystem ? Styles.LEFT_PILL : Styles.CENTER_PILL)
+                .styleClass(Styles.LEFT_PILL)
                 .grow(false, true);
 
         var fileBrowseButton = new ButtonComp(null, new FontIcon("mdi2f-folder-open-outline"), () -> {
-                    StandaloneFileBrowser.openSingleFile(() -> hideFileSystem ? fileSystem.getValue() : null, fileStore -> {
+                    StandaloneFileBrowser.openSingleFile(() -> fileSystem.getValue(), fileStore -> {
                         if (fileStore == null) {
                             filePath.setValue(null);
                             fileSystem.setValue(null);
@@ -66,7 +51,7 @@ public class FileReferenceChoiceComp extends SimpleComp {
                 .styleClass(Styles.RIGHT_PILL)
                 .grow(false, true);
 
-        var layout = new HorizontalComp(List.of(fileSystemChoiceComp, fileNameComp, fileBrowseButton))
+        var layout = new HorizontalComp(List.of(fileNameComp, fileBrowseButton))
                 .apply(struc -> struc.get().setFillHeight(true));
 
         layout.apply(struc -> {
