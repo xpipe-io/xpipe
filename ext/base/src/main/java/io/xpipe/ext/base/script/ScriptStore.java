886eb00a65bbb60d003f811039375d6ba3a1d4e4
crschnick <crschnick@xpipe.io>
2024-02-17 17:44:03 +0000

    Better explain stack overflow error

--- ext/base/src/main/java/io/xpipe/ext/base/script/ScriptStore.java
+++ ext/base/src/main/java/io/xpipe/ext/base/script/ScriptStore.java
@@ -50,6 +50,8 @@ public abstract class ScriptStore extends JacksonizedValue implements DataStore,
                 }
             });
             return pc;
+        }  catch (StackOverflowError t) {
+            throw new RuntimeException("Unable to set up scripts. Is there a circular script dependency?", t);
         } catch (Throwable t) {
             throw new RuntimeException("Unable to set up scripts", t);
         }
