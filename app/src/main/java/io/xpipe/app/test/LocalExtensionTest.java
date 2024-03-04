3d30613a5466d3aa76f104b4de6ba689b2eca09e
crschnick <crschnick@xpipe.io>
2024-03-04 14:42:52 +0000

    More test fixes

--- app/src/main/java/io/xpipe/app/test/LocalExtensionTest.java
+++ app/src/main/java/io/xpipe/app/test/LocalExtensionTest.java
@@ -1,6 +1,7 @@
 package io.xpipe.app.test;
 
 import io.xpipe.app.core.mode.OperationMode;
+import io.xpipe.core.process.OsType;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -13,6 +14,7 @@ public class LocalExtensionTest extends ExtensionTest {
             return;
         }
 
-        OperationMode.init(new String[0]);
+        var mode = OsType.getLocal().equals(OsType.WINDOWS) ? "tray" : "background";
+        OperationMode.init(new String[] {"--mode", mode});
     }
 }
