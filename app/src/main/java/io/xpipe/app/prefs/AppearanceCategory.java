529563aa24ccfefb691596579851a33532576275
crschnick <crschnick@xpipe.io>
2024-03-22 09:25:00 +0000

    Fix combo box popup jumping when switching themes

--- app/src/main/java/io/xpipe/app/prefs/AppearanceCategory.java
+++ app/src/main/java/io/xpipe/app/prefs/AppearanceCategory.java
@@ -23,7 +23,7 @@ public class AppearanceCategory extends AppPrefsCategory {
                 .addTitle("uiOptions")
                 .sub(new OptionsBuilder()
                         .nameAndDescription("theme")
-                        .addComp(ChoiceComp.ofTranslatable(prefs.theme, AppTheme.Theme.ALL, false), prefs.theme)
+                        .addComp(ChoiceComp.ofTranslatable(prefs.theme, AppTheme.Theme.ALL, false).styleClass("theme-switcher"), prefs.theme)
                         .nameAndDescription("performanceMode")
                         .addToggle(prefs.performanceMode)
                         .nameAndDescription("uiScale")
