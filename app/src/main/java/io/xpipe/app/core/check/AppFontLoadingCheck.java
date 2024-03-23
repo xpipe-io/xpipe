a07a18d4f05fc5e7ba73a22fc087b721f0142ae5
crschnick <crschnick@xpipe.io>
2024-03-23 13:31:22 +0000

    Improve font check

--- app/src/main/java/io/xpipe/app/core/check/AppFontLoadingCheck.java
+++ app/src/main/java/io/xpipe/app/core/check/AppFontLoadingCheck.java
@@ -1,5 +1,8 @@
 package io.xpipe.app.core.check;
 
+import io.xpipe.app.core.mode.OperationMode;
+import io.xpipe.app.issue.ErrorEvent;
+import io.xpipe.app.issue.LogErrorHandler;
 import javafx.scene.text.Font;
 
 public class AppFontLoadingCheck {
@@ -9,7 +12,11 @@ public class AppFontLoadingCheck {
             // This can fail if the found system fonts can somehow not be loaded
             Font.getDefault();
         } catch (Throwable e) {
-            throw new IllegalStateException("Unable to load fonts", e);
+            var event = ErrorEvent.fromThrowable("Unable to load fonts", e).build();
+            // We can't use the normal error handling facility
+            // as the platform reports as working but opening windows still does not work
+            new LogErrorHandler().handle(event);
+            OperationMode.halt(1);
         }
     }
 }
