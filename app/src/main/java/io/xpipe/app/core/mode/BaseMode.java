7e8d31dd3caefd46a29652a888c8f536388c391a
crschnick <crschnick@xpipe.io>
2024-03-27 13:28:13 +0000

    Rework setsid usage

--- app/src/main/java/io/xpipe/app/core/mode/BaseMode.java
+++ app/src/main/java/io/xpipe/app/core/mode/BaseMode.java
@@ -6,7 +6,6 @@ import io.xpipe.app.core.*;
 import io.xpipe.app.core.check.AppAvCheck;
 import io.xpipe.app.core.check.AppCertutilCheck;
 import io.xpipe.app.core.check.AppShellCheck;
-import io.xpipe.app.core.check.AppSidCheck;
 import io.xpipe.app.ext.ActionProvider;
 import io.xpipe.app.issue.TrackEvent;
 import io.xpipe.app.prefs.AppPrefs;
@@ -50,7 +49,6 @@ public class BaseMode extends OperationMode {
         AppPrefs.initLocal();
         AppCertutilCheck.check();
         AppAvCheck.check();
-        AppSidCheck.check();
         LocalShell.init();
         AppShellCheck.check();
         XPipeDistributionType.init();
