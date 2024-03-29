061dbe1cf39656b160e9d6e029193923d8757abf
crschnick <crschnick@xpipe.io>
2024-03-29 13:51:53 +0000

    Improve help command for linux/macos

--- app/src/main/java/io/xpipe/app/Main.java
+++ app/src/main/java/io/xpipe/app/Main.java
@@ -12,12 +12,15 @@ public class Main {
             return;
         }
 
-        // Since this is not marked as a console application, it will not print anything when you run it in a console
-        // So sadly there can't be a help command
-        //        if (args.length == 1 && args[0].equals("--help")) {
-        //            System.out.println("HELP");
-        //            return;
-        //        }
+        // Since this is not marked as a console application, it will not print anything when you run it in a console on Windows
+        if (args.length == 1 && args[0].equals("--help")) {
+            System.out.println("""
+                               The daemon executable xpiped does not accept any command-line arguments.
+                               
+                               For a reference on what you can do from the CLI, take a look at the xpipe CLI executable instead.
+                               """);
+            return;
+        }
 
         OperationMode.init(args);
     }
