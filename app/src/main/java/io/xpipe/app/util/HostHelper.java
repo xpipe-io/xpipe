b3e2878ff60f1bebdb0bdd6f49bc52550cb2e76a
crschnick <crschnick@xpipe.io>
2024-04-05 12:34:32 +0000

    Add method to find local free port

--- app/src/main/java/io/xpipe/app/util/HostHelper.java
+++ app/src/main/java/io/xpipe/app/util/HostHelper.java
@@ -1,8 +1,16 @@
 package io.xpipe.app.util;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.util.Locale;
 
 public class HostHelper {
+
+    public static int findRandomOpenPortOnAllLocalInterfaces() throws IOException {
+        try (ServerSocket socket = new ServerSocket(0)) {
+            return socket.getLocalPort();
+        }
+    }
 
     public static boolean isLocalHost(String host) {
         if (host.equals("127.0.0.1")) {
