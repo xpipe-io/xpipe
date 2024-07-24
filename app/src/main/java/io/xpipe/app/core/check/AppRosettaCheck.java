dc93536be99edd897e9ffb580b890a6d0ee3aa98
crschnick <crschnick@xpipe.io>
2024-07-24 11:18:13 +0000

    Add rosetta check

---
+++ app/src/main/java/io/xpipe/app/core/check/AppRosettaCheck.java
@@ -0,0 +1,26 @@
+package io.xpipe.app.core.check;
+
+import io.xpipe.app.core.AppProperties;
+import io.xpipe.app.issue.ErrorEvent;
+import io.xpipe.app.util.LocalShell;
+import io.xpipe.core.process.OsType;
+
+public class AppRosettaCheck {
+
+    public static void check() throws Exception {
+        if (OsType.getLocal() != OsType.MACOS) {
+            return;
+        }
+
+        if (!AppProperties.get().getArch().equals("x86_64")) {
+            return;
+        }
+
+        var ret = LocalShell.getShell().executeSimpleStringCommand("sysctl -n sysctl.proc_translated");
+        if (ret.equals("1")) {
+            ErrorEvent.fromMessage("You are running the Intel version of XPipe on an Apple Silicon system." +
+                    " There is a native build available that comes with much better performance." +
+                    " Please install that one instead.");
+        }
+    }
+}
