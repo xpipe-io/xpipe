ccb018a522a662c9cafb96e6857401db7c59930c
crschnick <crschnick@xpipe.io>
2024-07-27 10:55:04 +0000

    Style fixes

--- app/src/main/java/io/xpipe/app/fxcomps/impl/DataStoreChoiceComp.java
+++ app/src/main/java/io/xpipe/app/fxcomps/impl/DataStoreChoiceComp.java
@@ -114,22 +114,31 @@ public class DataStoreChoiceComp<T extends DataStore> extends SimpleComp {
                             StoreViewState.get().getActiveCategory(),
                             selectedCategory)
                     .styleClass(Styles.LEFT_PILL);
-            var filter =
-                    new FilterComp(filterText).styleClass(Styles.CENTER_PILL).hgrow();
+            var filter = new FilterComp(filterText).styleClass(Styles.CENTER_PILL).hgrow();
 
             var addButton = Comp.of(() -> {
                         MenuButton m = new MenuButton(null, new FontIcon("mdi2p-plus-box-outline"));
+                        m.setMaxHeight(100);
+                        m.setMinHeight(0);
                         StoreCreationMenu.addButtons(m);
                         return m;
                     })
                     .accessibleTextKey("addConnection")
-                    .padding(new Insets(-2))
-                    .styleClass(Styles.RIGHT_PILL)
-                    .grow(false, true);
+                    .padding(new Insets(-5))
+                    .styleClass(Styles.RIGHT_PILL);
 
-            var top = new HorizontalComp(List.of(category, filter.hgrow(), addButton))
+            var top = new HorizontalComp(List.of(category, filter, addButton))
                     .styleClass("top")
                     .apply(struc -> struc.get().setFillHeight(true))
+                    .apply(struc -> {
+                        var first = ((Region) struc.get().getChildren().get(0));
+                        var second = ((Region) struc.get().getChildren().get(1));
+                        var third = ((Region) struc.get().getChildren().get(1));
+                        second.prefHeightProperty().bind(first.heightProperty());
+                        second.minHeightProperty().bind(first.heightProperty());
+                        second.maxHeightProperty().bind(first.heightProperty());
+                        third.prefHeightProperty().bind(first.heightProperty());
+                    })
                     .apply(struc -> {
                         // Ugly solution to focus the text field
                         // Somehow this does not work through the normal on shown listeners
