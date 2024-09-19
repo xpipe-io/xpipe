0e5d107c3fcc57d264a0463a35aab98de03b3198
crschnick <crschnick@xpipe.io>
2024-09-19 16:37:16 +0000

    Password manager fixes

--- app/src/main/java/io/xpipe/app/prefs/PasswordManagerCategory.java
+++ app/src/main/java/io/xpipe/app/prefs/PasswordManagerCategory.java
@@ -4,23 +4,23 @@ import atlantafx.base.theme.Styles;
 import io.xpipe.app.comp.base.ButtonComp;
 import io.xpipe.app.comp.base.IntegratedTextAreaComp;
 import io.xpipe.app.core.AppI18n;
-import io.xpipe.app.ext.LocalStore;
 import io.xpipe.app.ext.ProcessControlProvider;
 import io.xpipe.app.fxcomps.Comp;
 import io.xpipe.app.fxcomps.impl.HorizontalComp;
+import io.xpipe.app.fxcomps.impl.LabelComp;
 import io.xpipe.app.fxcomps.impl.TextFieldComp;
 import io.xpipe.app.fxcomps.impl.VerticalComp;
 import io.xpipe.app.fxcomps.util.BindingsHelper;
 import io.xpipe.app.util.OptionsBuilder;
-import io.xpipe.app.util.TerminalLauncher;
 import io.xpipe.app.util.ThreadHelper;
-import io.xpipe.core.process.CommandBuilder;
-import io.xpipe.core.process.CommandControl;
+import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Region;
 import lombok.Value;
 import org.kordamp.ikonli.javafx.FontIcon;
 
@@ -41,28 +41,11 @@ public class PasswordManagerCategory extends AppPrefsCategory {
         return "passwordManager";
     }
 
-    private Comp<?> createTemplateChoice() {
-        return Comp.of(() -> {
-            var cb = new MenuButton();
-            cb.textProperty().bind(AppI18n.observable("templates"));
-            ExternalPasswordManagerTemplate.ALL.forEach(e -> {
-                var m = new MenuItem(
-                        e.toTranslatedString().getValue());
-                m.setOnAction(event -> {
-                    AppPrefs.get().passwordManagerCommand.set(e.getTemplate());
-                    event.consume();
-                });
-                cb.getItems().add(m);
-            });
-            return cb;
-        });
-    }
-
     @Override
     protected Comp<?> create() {
         var choices = new ArrayList<Choice>();
         ExternalPasswordManagerTemplate.ALL.forEach(externalPasswordManagerTemplate -> {
-            choices.add(new Choice(externalPasswordManagerTemplate.getId(), externalPasswordManagerTemplate.getTemplate(), null));
+            choices.add(new Choice(externalPasswordManagerTemplate.getId(), externalPasswordManagerTemplate.getTemplate(), ExternalPasswordManager.COMMAND));
         });
         ExternalPasswordManager.ALL.stream().filter(externalPasswordManager -> externalPasswordManager != ExternalPasswordManager.COMMAND).forEach(externalPasswordManager -> {
             choices.add(new Choice(externalPasswordManager.getId(), null, externalPasswordManager));
@@ -70,21 +53,23 @@ public class PasswordManagerCategory extends AppPrefsCategory {
 
         var prefs = AppPrefs.get();
         var testPasswordManagerValue = new SimpleStringProperty();
+        var testPasswordManagerResult = new SimpleStringProperty();
         Runnable test = () -> {
-            var cmd = prefs.passwordManagerString(testPasswordManagerValue.get());
-            if (cmd == null) {
-                return;
-            }
-
             ThreadHelper.runFailableAsync(() -> {
-                TerminalLauncher.open(
-                        "Password test",
-                        new LocalStore()
-                                .control()
-                                .command(CommandBuilder.ofFunction(sc -> cmd
-                                        + "\n"
-                                        + sc.getShellDialect().getEchoCommand("Is this your password?", false)))
-                                .terminalExitMode(CommandControl.TerminalExitMode.KEEP_OPEN));
+                if (prefs.passwordManager.getValue() == null) {
+                    return;
+                }
+
+                var r = prefs.passwordManager.getValue().retrievePassword(testPasswordManagerValue.get());
+                Platform.runLater(() -> {
+                    testPasswordManagerResult.set(r);
+                    ThreadHelper.runAsync(() -> {
+                        ThreadHelper.sleep(5000);
+                        Platform.runLater(() -> {
+                            testPasswordManagerResult.set(null);
+                        });
+                    });
+                });
             });
         };
 
@@ -99,12 +84,13 @@ public class PasswordManagerCategory extends AppPrefsCategory {
                     struc.getTextArea().setPromptText("mypassmgr get $KEY");
                 })
                 .disable(prefs.passwordManagerCommand.isNull())
+                .hide(prefs.passwordManagerCommand.isNull())
                 .minWidth(350)
                 .minHeight(120);
         var templates = Comp.of(() -> {
             var cb = new MenuButton();
             cb.textProperty().bind(BindingsHelper.flatMap(prefs.passwordManager,externalPasswordManager -> {
-                return externalPasswordManager != null ? AppI18n.observable(externalPasswordManager.getId()) : AppI18n.observable("templates");
+                return externalPasswordManager != null ? AppI18n.observable(externalPasswordManager.getId()) : AppI18n.observable("selectType");
             }));
             choices.forEach(e -> {
                 var m = new MenuItem();
@@ -123,15 +109,28 @@ public class PasswordManagerCategory extends AppPrefsCategory {
             struc.get().setSpacing(10);
         });
 
-        var testPasswordManager = new HorizontalComp(List.of(
-                        new TextFieldComp(testPasswordManagerValue)
-                                .apply(struc -> struc.get().setPromptText("Enter password key"))
-                                .styleClass(Styles.LEFT_PILL)
-                                .grow(false, true),
-                        new ButtonComp(null, new FontIcon("mdi2p-play"), test)
-                                .styleClass(Styles.RIGHT_PILL)
-                                .grow(false, true)))
-                .padding(new Insets(15, 0, 0, 0))
+        var testInput = new HorizontalComp(List.<Comp<?>>of(
+                new TextFieldComp(testPasswordManagerValue)
+                        .apply(struc -> struc.get().setPromptText("Enter password key"))
+                        .styleClass(Styles.LEFT_PILL)
+                        .apply(struc -> struc.get().setOnKeyPressed(event -> {
+                            if (event.getCode() == KeyCode.ENTER) {
+                                test.run();
+                                event.consume();
+                            }
+                        })),
+                new ButtonComp(null, new FontIcon("mdi2p-play"), test)
+                        .styleClass(Styles.RIGHT_PILL)));
+        testInput.apply(struc -> {
+            var first = ((Region) struc.get().getChildren().get(0));
+            var second = ((Region) struc.get().getChildren().get(1));
+            second.prefHeightProperty().bind(first.heightProperty());
+        });
+
+        var testPasswordManager = new HorizontalComp(List.of(testInput,
+                        Comp.hspacer(25),
+                        new LabelComp(testPasswordManagerResult).apply(struc -> struc.get().setOpacity(0.5))))
+                .padding(new Insets(10, 0, 0, 0))
                 .apply(struc -> struc.get().setAlignment(Pos.CENTER_LEFT))
                 .apply(struc -> struc.get().setFillHeight(true));
         return new OptionsBuilder()
