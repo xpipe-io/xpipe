ca0df83c91ce0e353555b5dd23feedb16c2af2e4
crschnick <crschnick@xpipe.io>
2024-09-27 13:01:38 +0000

    Improve options check

--- app/src/main/java/io/xpipe/app/core/check/AppJavaOptionsCheck.java
+++ app/src/main/java/io/xpipe/app/core/check/AppJavaOptionsCheck.java
@@ -11,9 +11,10 @@ public class AppJavaOptionsCheck {
         }
 
         ErrorEvent.fromMessage(
-                        "You have configured the environment variable _JAVA_OPTIONS=%s on your system.".formatted(env)
-                                + " This will forcefully apply all custom JVM options to XPipe as well and can cause a variety of different issues."
+                        "You have configured the global environment variable _JAVA_OPTIONS=%s on your system.".formatted(env)
+                                + " This will forcefully apply all custom JVM options to XPipe and can cause a variety of different issues."
                                 + " Please remove this global environment variable and use local configuration instead for your other JVM programs.")
+                .noDefaultActions()
                 .expected()
                 .handle();
     }
