8bb47fa4d480772a9fc79954fdd860d782d07e5f
crschnick <crschnick@xpipe.io>
2024-10-30 17:54:52 +0000

    Rework

--- app/src/main/java/io/xpipe/app/comp/store/StoreCreationMenu.java
+++ app/src/main/java/io/xpipe/app/comp/store/StoreCreationMenu.java
@@ -33,11 +33,9 @@ public class StoreCreationMenu {
         menu.getItems().add(category("addDesktop", "mdi2c-camera-plus", DataStoreCreationCategory.DESKTOP, null));
 
         menu.getItems()
-                .add(category(
-                        "addShell", "mdi2t-text-box-multiple", DataStoreCreationCategory.SHELL, "shellEnvironment"));
-
-        menu.getItems()
                 .add(category("addScript", "mdi2s-script-text-outline", DataStoreCreationCategory.SCRIPT, "script"));
+
+        menu.getItems().add(category("addCommand", "mdi2c-code-greater-than", DataStoreCreationCategory.COMMAND, null));
 
         menu.getItems()
                 .add(category(
