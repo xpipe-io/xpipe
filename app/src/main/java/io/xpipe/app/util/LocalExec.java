efc06714fef9f3b881e519933759d6cfb3dcb59d
crschnick <crschnick@xpipe.io>
2025-02-08 21:06:38 +0000

    Fixes [stage]

--- app/src/main/java/io/xpipe/app/util/LocalExec.java
+++ app/src/main/java/io/xpipe/app/util/LocalExec.java
@@ -2,14 +2,18 @@ package io.xpipe.app.util;
 
 import io.xpipe.app.core.AppProperties;
 import io.xpipe.app.issue.ErrorEvent;
+import io.xpipe.app.issue.TrackEvent;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class LocalExec {
 
     public static Optional<String> readStdoutIfPossible(String... command) {
         try {
+            TrackEvent.withTrace("Running local command").tag("command", String.join(" ", command)).handle();
             var process = new ProcessBuilder(command)
                     .redirectError(ProcessBuilder.Redirect.DISCARD)
                     .start();
@@ -18,7 +22,9 @@ public class LocalExec {
             if (process.exitValue() != 0) {
                 return Optional.empty();
             } else {
-                return Optional.of(new String(out, StandardCharsets.UTF_8));
+                var s = new String(out, StandardCharsets.UTF_8);
+                TrackEvent.withTrace("Local command finished").tag("command", String.join(" ", command)).tag("stdout", s).handle();
+                return Optional.of(s);
             }
         } catch (Exception ex) {
             ErrorEvent.fromThrowable(ex).handle();
