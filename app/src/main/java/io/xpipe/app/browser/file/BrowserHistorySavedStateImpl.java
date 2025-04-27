f0847162592ad0641dbfd47f8db14b29620a4d6a
crschnick <crschnick@xpipe.io>
2025-04-27 13:32:46 +0000

    Filter invalid history state

--- app/src/main/java/io/xpipe/app/browser/file/BrowserHistorySavedStateImpl.java
+++ app/src/main/java/io/xpipe/app/browser/file/BrowserHistorySavedStateImpl.java
@@ -82,7 +82,8 @@ public class BrowserHistorySavedStateImpl implements BrowserHistorySavedState {
             if (ls == null) {
                 ls = List.of();
             }
-            return new BrowserHistorySavedStateImpl(ls);
+            var valid = ls.stream().filter(entry -> entry.getUuid() != null && entry.getPath() != null).toList();
+            return new BrowserHistorySavedStateImpl(valid);
         }
     }
 }
