462fdc1cae24094785eb54f53a15d291ef7fc732
crschnick <crschnick@xpipe.io>
2025-05-18 14:46:21 +0000

    Fix link

--- app/src/main/java/io/xpipe/app/util/DocumentationLink.java
+++ app/src/main/java/io/xpipe/app/util/DocumentationLink.java
@@ -13,7 +13,7 @@ public enum DocumentationLink {
     EULA("legal/eula"),
     WEBTOP_UPDATE("guide/webtop#updating"),
     SYNC("guide/sync"),
-    GETTING_STARTED("guide/getting-started"),
+    FIRST_STEPS("guide/first-steps"),
     DESKTOP_APPLICATIONS("guide/desktop-applications"),
     SERVICES("guide/services"),
     SCRIPTING("guide/scripting"),
