11c1a0f73d0370651046497e971ac5d8b01b38ce
crschnick <crschnick@xpipe.io>
2025-06-03 23:59:07 +0000

    Fix remmina

--- app/src/main/java/io/xpipe/app/vnc/ExternalVncClient.java
+++ app/src/main/java/io/xpipe/app/vnc/ExternalVncClient.java
@@ -33,7 +33,7 @@ public interface ExternalVncClient {
         l.add(InternalVncClient.class);
         switch (OsType.getLocal()) {
             case OsType.Linux linux -> {
-                l.add(RemminaHelper.class);
+                l.add(RealVncClient.class);
                 l.add(RealVncClient.Linux.class);
                 l.add(TigerVncClient.Linux.class);
             }
