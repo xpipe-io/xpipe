49583b350fa4a958c0773862b144c41ed79c5835
crschnick <crschnick@xpipe.io>
2025-06-13 11:56:59 +0000

    Rework icons

--- app/src/main/java/io/xpipe/app/comp/base/AppMainWindowContentComp.java
+++ app/src/main/java/io/xpipe/app/comp/base/AppMainWindowContentComp.java
@@ -8,6 +8,7 @@ import io.xpipe.app.core.AppResources;
 import io.xpipe.app.core.window.AppDialog;
 import io.xpipe.app.core.window.AppMainWindow;
 import io.xpipe.app.issue.TrackEvent;
+import io.xpipe.app.prefs.AppPrefs;
 import io.xpipe.app.util.PlatformThread;
 import io.xpipe.core.process.OsType;
 
@@ -16,10 +17,12 @@ import javafx.beans.property.SimpleBooleanProperty;
 import javafx.collections.ListChangeListener;
 import javafx.css.PseudoClass;
 import javafx.geometry.Pos;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
@@ -40,27 +43,50 @@ public class AppMainWindowContentComp extends SimpleComp {
         var sidebarPresent = new SimpleBooleanProperty();
         var bg = Comp.of(() -> {
             var loadingIcon = new ImageView();
-            loadingIcon.setFitWidth(64);
-            loadingIcon.setFitHeight(64);
+            loadingIcon.setFitWidth(128);
+            loadingIcon.setFitHeight(128);
 
-            var anim = Animations.pulse(loadingIcon, 1.1);
-            if (OsType.getLocal() != OsType.LINUX) {
-                anim.setRate(0.85);
-                anim.setCycleCount(Animation.INDEFINITE);
-                anim.play();
-            }
+            var color = AppPrefs.get() != null && AppPrefs.get().theme().getValue().isDark() ?
+                    Color.web("#0b898aff") : Color.web("#0b898aff");
+            DropShadow shadow = new DropShadow();
+            shadow.setRadius(10);
+            shadow.setColor(color);
+
+            var loadingAnimation = new AnimationTimer() {
+
+                long offset;
+
+                @Override
+                public void handle(long now) {
+                    // Increment offset as we are always having 60fps
+                    // Prevents animation jumps when the animation timer isn't called for a long time
+                    offset += 1000 / 60;
+
+                    // Move shadow in a circle
+                    var rad = -(offset % 1000.0) / 1000.0 * 2 * Math.PI;
+                    var x = Math.sin(rad);
+                    var y = Math.cos(rad);
+                    shadow.setOffsetX(x * 3);
+                    shadow.setOffsetY(y * 3);
+                }
+            };
+
+            loadingIcon.setEffect(shadow);
+            loadingAnimation.start();
 
             // This allows for assigning logos even if AppImages has not been initialized yet
             var dir = "img/logo/";
             AppResources.with(AppResources.XPIPE_MODULE, dir, path -> {
-                loadingIcon.setImage(AppImages.loadImage(path.resolve("loading.png")));
+                var image = AppPrefs.get() != null && AppPrefs.get().theme().getValue().isDark() ?
+                        path.resolve("loading-dark.png") : path.resolve("loading.png");
+                loadingIcon.setImage(AppImages.loadImage(image));
             });
 
             var version = new LabelComp((AppProperties.get().isStaging() ? "XPipe PTB" : "XPipe") + " "
                     + AppProperties.get().getVersion());
             version.apply(struc -> {
-                AppFontSizes.xxl(struc.get());
-                struc.get().setOpacity(0.6);
+                AppFontSizes.apply(struc.get(), appFontSizes -> "15");
+                struc.get().setOpacity(0.65);
             });
 
             var text = new LabelComp(AppMainWindow.getInstance().getLoadingText());
@@ -86,13 +112,13 @@ public class AppMainWindowContentComp extends SimpleComp {
                 if (struc != null) {
                     TrackEvent.info("Window content node set");
                     PlatformThread.runNestedLoopIteration();
-                    anim.stop();
                     struc.prepareAddition();
                     pane.getChildren().add(struc.get());
                     sidebarPresent.set(true);
                     PlatformThread.runNestedLoopIteration();
                     pane.getStyleClass().remove("background");
                     pane.getChildren().remove(vbox);
+                    loadingAnimation.stop();
                     struc.show();
                     TrackEvent.info("Window content node shown");
                 }
