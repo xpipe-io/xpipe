9241d5d07533173f544ce8b9922fe82b2275160c
crschnick <crschnick@xpipe.io>
2025-06-22 19:06:18 +0000

    Fixes

--- app/src/main/java/io/xpipe/app/issue/ErrorAction.java
+++ app/src/main/java/io/xpipe/app/issue/ErrorAction.java
@@ -2,6 +2,9 @@ package io.xpipe.app.issue;
 
 import io.xpipe.app.core.AppI18n;
 import io.xpipe.app.util.Hyperlinks;
+import io.xpipe.core.util.FailableSupplier;
+
+import java.util.function.Supplier;
 
 public interface ErrorAction {
 
@@ -25,33 +28,54 @@ public interface ErrorAction {
         };
     }
 
-    static ErrorAction ignore() {
+    static ErrorAction translated(String key, FailableSupplier<Boolean> r) {
         return new ErrorAction() {
             @Override
             public String getName() {
-                return AppI18n.get("ignoreError");
+                return AppI18n.get(key);
             }
 
             @Override
             public String getDescription() {
-                return AppI18n.get("ignoreErrorDescription");
+                return AppI18n.get(key + "Description");
             }
 
             @Override
-            public boolean handle(ErrorEvent event) {
-                if (!event.isReportable()) {
-                    return true;
-                }
-
-                SentryErrorHandler.getInstance().handle(event);
-                return true;
+            public boolean handle(ErrorEvent event) throws Exception {
+                return r.get();
             }
         };
+    }
+
+    static IgnoreAction ignore() {
+        return new IgnoreAction();
     }
 
     String getName();
 
     String getDescription();
 
-    boolean handle(ErrorEvent event);
+    boolean handle(ErrorEvent event) throws Exception;
+
+    class IgnoreAction implements ErrorAction {
+        @Override
+        public String getName() {
+            return AppI18n.get("ignoreError");
+        }
+
+        @Override
+        public String getDescription() {
+            return AppI18n.get("ignoreErrorDescription");
+        }
+
+        @Override
+        public boolean handle(ErrorEvent event) {
+            if (!event.isReportable()) {
+                return true;
+            }
+
+            SentryErrorHandler.getInstance().handle(event);
+            return true;
+        }
+    }
 }
