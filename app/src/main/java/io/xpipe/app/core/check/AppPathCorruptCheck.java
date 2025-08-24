42460f2c81015343da57413396b9076c2b682722
crschnick <crschnick@xpipe.io>
2025-08-24 18:05:15 +0000

    Improve corrupt path message

--- app/src/main/java/io/xpipe/app/core/check/AppPathCorruptCheck.java
+++ app/src/main/java/io/xpipe/app/core/check/AppPathCorruptCheck.java
@@ -18,7 +18,7 @@ public class AppPathCorruptCheck {
 
         ErrorEventFactory.fromMessage(
                         "Your system PATH looks to be corrupt, essential system tools are not available. This will cause XPipe to not function "
-                                + "correctly. Please make sure to fix your PATH environment variable to include the base Windows tools.")
+                                + "correctly. Please fix your PATH environment variable to include the base Windows tool directories like C:\\Windows\\system32 and others.")
                 .expected()
                 .handle();
     }
