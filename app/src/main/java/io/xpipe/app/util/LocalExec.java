5f2b3094611fc06bb8bde3c5c0747c9c38320b19
crschnick <crschnick@xpipe.io>
2025-11-02 23:26:20 +0000

    Small fixes

--- app/src/main/java/io/xpipe/app/util/LocalExec.java
+++ app/src/main/java/io/xpipe/app/util/LocalExec.java
@@ -1,11 +1,36 @@
 package io.xpipe.app.util;
 
+import io.xpipe.app.core.AppSystemInfo;
 import io.xpipe.app.issue.TrackEvent;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 public class LocalExec {
+
+    public static void executeAsync(String... command) {
+        try {
+            TrackEvent.withTrace("Running local command")
+                    .tag("command", String.join(" ", command))
+                    .handle();
+
+            var pb = new ProcessBuilder(command)
+                    .redirectOutput(ProcessBuilder.Redirect.DISCARD)
+                    .redirectError(ProcessBuilder.Redirect.DISCARD);
+            pb.directory(AppSystemInfo.ofCurrent().getUserHome().toFile());
+
+            var env = pb.environment();
+            // https://bugs.openjdk.org/browse/JDK-8360500
+            env.remove("_JPACKAGE_LAUNCHER");
+
+            pb.start();
+        } catch (Exception ex) {
+            TrackEvent.withTrace("Local command finished")
+                    .tag("command", String.join(" ", command))
+                    .tag("error", ex.toString())
+                    .handle();
+        }
+    }
 
     public static Optional<String> readStdoutIfPossible(String... command) {
         try {
@@ -14,6 +39,8 @@ public class LocalExec {
                     .handle();
 
             var pb = new ProcessBuilder(command).redirectError(ProcessBuilder.Redirect.DISCARD);
+            pb.directory(AppSystemInfo.ofCurrent().getUserHome().toFile());
+
             var env = pb.environment();
             // https://bugs.openjdk.org/browse/JDK-8360500
             env.remove("_JPACKAGE_LAUNCHER");
