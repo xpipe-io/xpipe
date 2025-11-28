f92f321c890993164e63223b929c585d69f064a9
crschnick <crschnick@xpipe.io>
2025-11-28 02:30:48 +0000

    Fix broken docs link

--- app/src/main/java/io/xpipe/app/util/DocumentationLink.java
+++ app/src/main/java/io/xpipe/app/util/DocumentationLink.java
@@ -15,7 +15,7 @@ public enum DocumentationLink {
     PRIVACY("legal/privacy"),
     EULA("legal/eula"),
     WEBTOP_UPDATE("guide/webtop#updating"),
-    WEBTOP_TUN("guide/webtop##networking-tailscale-and-netbird"),
+    WEBTOP_TUN("guide/webtop#networking-tailscale-and-netbird"),
     SYNC("guide/sync"),
     SYNC_LOCAL("guide/sync#local-repositories"),
     DESKTOP_APPLICATIONS("guide/desktop-applications"),
