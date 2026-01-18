c9910ded405c474590396dfed628bf01e271e66b
crschnick <crschnick@xpipe.io>
2026-01-18 23:58:28 +0000

    Fixes

--- app/src/main/java/io/xpipe/app/hub/comp/OsLogoComp.java
+++ app/src/main/java/io/xpipe/app/hub/comp/OsLogoComp.java
@@ -66,6 +66,7 @@ public class OsLogoComp extends SimpleComp {
             AppResources.with(AppResources.MAIN_MODULE, "os", file -> {
                 try (var list = Files.list(file)) {
                     list.filter(path -> path.toString().endsWith(".png")
+                                    && !path.toString().contains("-dark")
                                     && !path.toString().endsWith(LINUX_DEFAULT_24)
                                     && !path.toString().endsWith("-40.png"))
                             .map(path -> path.getFileName().toString())
