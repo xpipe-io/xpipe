91e05e9f8e2c1bd2620305e8dc8cd0d0117dde85
crschnick <crschnick@xpipe.io>
2026-02-18 15:46:15 +0000

    Various fixes [stage]

--- app/src/main/java/io/xpipe/app/prefs/McpCategory.java
+++ app/src/main/java/io/xpipe/app/prefs/McpCategory.java
@@ -1,7 +1,9 @@
 package io.xpipe.app.prefs;
 
+import atlantafx.base.theme.Styles;
 import io.xpipe.app.beacon.AppBeaconServer;
 import io.xpipe.app.comp.BaseRegionBuilder;
+import io.xpipe.app.comp.RegionBuilder;
 import io.xpipe.app.comp.base.IntegratedTextAreaComp;
 import io.xpipe.app.comp.base.TextAreaComp;
 import io.xpipe.app.core.AppI18n;
@@ -11,6 +13,10 @@ import io.xpipe.app.platform.OptionsBuilder;
 
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
 
 public class McpCategory extends AppPrefsCategory {
 
@@ -24,26 +30,11 @@ public class McpCategory extends AppPrefsCategory {
         return new LabelGraphic.IconGraphic("mdi2c-chat-processing-outline");
     }
 
-    @Override
-    protected BaseRegionBuilder<?, ?> create() {
+    private ObservableValue<String> createMcpConfig(String format) {
         var prefs = AppPrefs.get();
-
-        var mcpConfig = Bindings.createStringBinding(
+        return Bindings.createStringBinding(
                 () -> {
-                    var template = """
-                           {
-                             "mcpServers": {
-                               "%s": {
-                                 "type": "streamable-http",
-                                 "url": "http://localhost:%s/mcp",
-                                 "headers": {
-                                   "Authorization": "Bearer %s"
-                                 }
-                               }
-                             }
-                           }
-                           """;
-                    return template.formatted(
+                    return format.formatted(
                                     AppNames.ofCurrent().getKebapName(),
                                     AppBeaconServer.get().getPort(),
                                     prefs.apiKey().get() != null
@@ -52,26 +43,109 @@ public class McpCategory extends AppPrefsCategory {
                             .strip();
                 },
                 prefs.apiKey());
-        var mcpConfigProp = new SimpleStringProperty();
-        mcpConfigProp.bind(mcpConfig);
+    }
+
+    @Override
+    protected BaseRegionBuilder<?, ?> create() {
+        var prefs = AppPrefs.get();
+
+        var vsCodeTemplate = createMcpConfig("""
+             {
+               "servers": {
+                 "%s": {
+                   "type": "http",
+                   "url": "http://localhost:%s/mcp",
+                   "headers": {
+                     "Authorization": "Bearer %s"
+                   }
+                 }
+               }
+             }
+             """);
+
+        var cursorTemplate = createMcpConfig("""
+               {
+                 "mcpServers": {
+                   "%s": {
+                     "type": "streamable-http",
+                     "url": "http://localhost:%s/mcp",
+                     "headers": {
+                       "Authorization": "Bearer %s"
+                     }
+                   }
+                 }
+               }
+               """);
+
+        var warpTemplate = createMcpConfig("""
+               {
+                 "%s": {
+                   "serverUrl": "http://localhost:%s/mcp",
+                   "headers": {
+                     "Authorization": "Bearer %s"
+                   }
+                 }
+               }
+               """);
+
+        var tabComp = RegionBuilder.of(() -> {
+            var vsCode = new TextArea();
+            vsCode.setEditable(false);
+            vsCode.textProperty().bind(vsCodeTemplate);
+            vsCode.setPrefRowCount(12);
+            var vsCodeTab = new Tab();
+            vsCodeTab.textProperty().bind(AppI18n.observable("vscode"));
+            vsCodeTab.setContent(vsCode);
+            vsCodeTab.setClosable(false);
+
+            var cursor = new TextArea();
+            cursor.setEditable(false);
+            cursor.textProperty().bind(cursorTemplate);
+            cursor.setPrefRowCount(12);
+            var cursorTab = new Tab();
+            cursorTab.textProperty().bind(AppI18n.observable("cursor"));
+            cursorTab.setContent(cursor);
+            cursorTab.setClosable(false);
+
+            var warp = new TextArea();
+            warp.setEditable(false);
+            warp.textProperty().bind(warpTemplate);
+            warp.setPrefRowCount(12);
+            var warpTab = new Tab();
+            warpTab.textProperty().bind(AppI18n.observable("warp"));
+            warpTab.setContent(warp);
+            warpTab.setClosable(false);
+
+            var claude = new TextArea();
+            claude.setEditable(false);
+            claude.textProperty().bind(vsCodeTemplate);
+            claude.setPrefRowCount(12);
+            var claudeTab = new Tab();
+            claudeTab.textProperty().bind(AppI18n.observable("claude"));
+            claudeTab.setContent(claude);
+            claudeTab.setClosable(false);
+
+            var tabPane = new TabPane();
+            tabPane.getTabs().addAll(vsCodeTab, cursorTab, warpTab, claudeTab);
+            return tabPane;
+        });
 
         return new OptionsBuilder()
                 .addTitle("mcpServer")
                 .sub(new OptionsBuilder()
                         .pref(prefs.enableMcpServer)
                         .addToggle(prefs.enableMcpServer)
+                        .nameAndDescription("mcpClientConfigurationDetails")
+                        .addComp(tabComp)
+                        .hide(prefs.enableMcpServer.not())
                         .pref(prefs.enableMcpMutationTools)
                         .addToggle(prefs.enableMcpMutationTools)
-                        .nameAndDescription("mcpClientConfigurationDetails")
-                        .addComp(new TextAreaComp(mcpConfigProp).applyStructure(struc -> {
-                            struc.getTextArea().setEditable(false);
-                            struc.getTextArea().setPrefRowCount(12);
-                        }))
                         .hide(prefs.enableMcpServer.not())
                         .pref(prefs.mcpAdditionalContext)
                         .addComp(new IntegratedTextAreaComp(prefs.mcpAdditionalContext, false, "prompt", new SimpleStringProperty("txt")).applyStructure(structure -> {
                             structure.getTextArea().promptTextProperty().bind(AppI18n.observable("mcpAdditionalContextSample"));
                         }))
+                        .hide(prefs.enableMcpServer.not())
                 )
                 .buildComp();
     }
